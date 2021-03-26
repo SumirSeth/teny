ea2db64cdbe5bb300e1e47d57b556d8a98c9aa58
Sumir <clashroyalesumir@gmail.com>
2021-03-26 09:07:23 +0000

    added 24/7

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from keep_alive import keep_alive
 import os
 import discord
 from discord.ext import commands
@@ -5,7 +6,7 @@ from discord.ext import commands
 #------------------CONFIGS------------------
 token = os.environ['TOKEN']
 prefix = '.'
-bot = commands.Bot(command_prefix=prefix)
+bot = commands.Bot(command_prefix=prefix, help_command=None)
 owner = [740845704326676493, 575263293015588867]
 #------------------CONFIGS------------------
 def err(msg):
@@ -22,9 +23,19 @@ async def on_ready():
 
 
 #------------------COMMANDS------------------
-
+@bot.group(invoke_without_command=True)
+async def help(ctx):
+  e = discord.Embed(title="Help", description=f"Type {prefix}help <command> for more info.", color=ctx.author.color)
+  e.add_field(name="General", value=f"{prefix}fact", inline=True)
+  await ctx.send(embed=e)
+@help.command()
+async def fact(ctx):
+  e = discord.Embed(title="Fact!", description="Gives a random fact each time!", color=ctx.author.color)
+  e.add_field(name="**Syntax**", value=f"`{prefix}fact`")
+  await ctx.send(embed=e)
 
 
 
 #------------------RUN------------------
+keep_alive()
 bot.run(token)
