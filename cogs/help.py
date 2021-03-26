b25aab6fed9a167e1a10c38e710462adcb1d7cfa
Sumir <clashroyalesumir@gmail.com>
2021-03-26 14:20:52 +0000

    added country cog help

--- cogs/help.py
+++ cogs/help.py
@@ -16,14 +16,18 @@ class Help(commands.Cog):
   @commands.group(invoke_without_command=True)
   async def help(self, ctx):
     e = discord.Embed(title="Help", description=f"Type {prefix}help <command> for more info.", color=ctx.author.color)
-    e.add_field(name="General", value=f"{prefix}fact", inline=True)
+    e.add_field(name="General", value=f"`{prefix}fact`, `{prefix}country`", inline=True)
     await ctx.send(embed=e)
   @help.command()
   async def fact(self, ctx):
     e = discord.Embed(title="Fact!", description=f"A category filled with various types of facts!\nType {prefix}fact for more info.", color=ctx.author.color)
-    e.add_field(name="**Syntax**", value=f"`{prefix}fact`")
+    e.add_field(name="**Syntax**", value=f"`{prefix}fact <category>`")
     await ctx.send(embed=e)
-  
+  @help.command()
+  async def country(self, ctx):
+    e = discord.Embed(title="Country!", description=f"Get any countries info!", color=ctx.author.color)
+    e.add_field(name="**Syntax**", value=f"`{prefix}country <name>`")
+    await ctx.send(embed=e)
 
 
 
