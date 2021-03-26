3dadbceace53f4f591e37d2890fcb1173fdda132
Sumir <clashroyalesumir@gmail.com>
2021-03-26 07:14:43 +0000

    Added bad embed function

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ prefix = '.'
 bot = commands.Bot(command_prefix=prefix)
 owner = [740845704326676493, 575263293015588867]
 #------------------CONFIGS------------------
-
+def err(msg):
+  er = discord.Embed(title="", description='', color=0xe74c3c)
+  er.set_author(name=msg,icon_url="https://cdn.discordapp.com/attachments/804212727869866006/824902921505865749/772509.png")
+  return er
 
 #------------------COMMANDS------------------
 
